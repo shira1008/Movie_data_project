100ec4701da91f0c3eb909062b20adaeb704cb11
shira1008 <shaharshira1008@gmail.com>
2023-05-30 14:43:53 +0300

    added storage folder

--- istorage.py
+++ istorage.py
@@ -4,52 +4,49 @@ from abc import ABC, abstractmethod
 class IStorage(ABC):
     @abstractmethod
     def list_movies(self):
-      """
-      Returns a dictionary of dictionaries that
-      contains the movies information in the database.
+        """
+        Returns a dictionary of dictionaries that
+        contains the movies information in the database.
 
-      The function loads the information from the JSON
-      file and returns the data.
+        The function loads the information from the JSON
+        file and returns the data.
 
-      For example, the function may return:
-      {
-        "Titanic": {
-          "rating": 9,
-          "year": 1999
-        },
-        "..." {
-          ...
-        },
-      }
-      """
-      pass
-
+        For example, the function may return:
+        {
+          "Titanic": {
+            "rating": 9,
+            "year": 1999
+          },
+          "..." {
+            ...
+          },
+        }
+        """
+        pass
 
     @abstractmethod
     def add_movie(self, title, year, rating, poster):
-      """
-      Adds a movie to the movies database.
-      Loads the information from the JSON file, adds the movie,
-      and saves it. The function doesn't need to validate the input.
-      """
-      pass
-
+        """
+        Adds a movie to the movies database.
+        Loads the information from the JSON file, adds the movie,
+        and saves it. The function doesn't need to validate the input.
+        """
+        pass
 
     @abstractmethod
     def delete_movie(self, title):
-      """
-      Deletes a movie from the movies database.
-      Loads the information from the JSON file, deletes the movie,
-      and saves it. The function doesn't need to validate the input.
-      """
-      pass
-      
+        """
+        Deletes a movie from the movies database.
+        Loads the information from the JSON file, deletes the movie,
+        and saves it. The function doesn't need to validate the input.
+        """
+        pass
 
     @abstractmethod
     def update_movie(self, title, notes):
-      """
-      Updates a movie from the movies database.
-      Loads the information from the JSON file, updates the movie,
-      and saves it. The function doesn't need to validate the input.
-      """
-      pass
+        """
+        Updates a movie from the movies database.
+        Loads the information from the JSON file, updates the movie,
+        and saves it. The function doesn't need to validate the input.
+        """
+        pass
--- main.py
+++ main.py
@@ -6,17 +6,17 @@ from storage_csv import StorageCsv
 
 def main():
     ##json storage:
-    storage = StorageJson('data.json')
+    storage = StorageJson('storage/data.json')
     movies = storage.list_movies()
     movie_app = MovieApp(storage)
     movie_app.run()
 
     ###csv storage:
-    # storage = StorageCsv('movies.csv')
+    # storage = StorageCsv('storage/movies.csv')
     # movie_app = MovieApp(storage)
     # movie_app.run()
-    
+
 
 if __name__ == "__main__":
   main()
-  
+
